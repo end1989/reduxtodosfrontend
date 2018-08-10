abd2b293352b384a09254290583220c1dfc3851a
Eldon Dahlin <end1989@gmail.com>
2018-08-09 18:48:46 -0600

    Todo list front end

---
+++ src/Todo.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const Todo = ({ task, removeTodo }) => (
+    <li>
+        {task} <button onClick={removeTodo}>X</button>
+    </li>
+);
+
+export default Todo;
